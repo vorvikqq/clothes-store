bba2a054ad71c8e8dcfe840b1c882879309fd751
vorvikqq <delodonn@ukr.net>
2025-04-25 21:47:15 +0300

    fix docker problem

--- clothes_store/settings.py
+++ clothes_store/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure--kh^v@vxhyo%uha1v9lx4pm-tj!pc71!bxz$5u-lrn9tansd&l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'web']
 
 
 # Application definition

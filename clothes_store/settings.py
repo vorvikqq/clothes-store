fe4dc98fa9f492fa3f1139759e4bae7484ea7552
vorvikqq <delodonn@ukr.net>
2025-04-27 20:21:06 +0300

    Change DEBUG True to False

--- clothes_store/settings.py
+++ clothes_store/settings.py
@@ -31,7 +31,7 @@ load_dotenv(BASE_DIR / '.env')
 SECRET_KEY = 'django-insecure--kh^v@vxhyo%uha1v9lx4pm-tj!pc71!bxz$5u-lrn9tansd&l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'web', 'clothes-store-331i.onrender.com']
 
